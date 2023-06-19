76e420981f3204c78f5f6eaf20db0b4d989e9da5
yemiflora <yemiflora20@gmail.com>
2023-06-19 11:40:06 -0700

    task update-1

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This  is the program entry point
+ *
+ * returns 0 as output
+ */
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
