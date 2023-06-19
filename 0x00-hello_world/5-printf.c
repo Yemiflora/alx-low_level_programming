ed93754b34e472845d76e0b576a12abfe261c756
yemiflora <yemiflora20@gmail.com>
2023-06-19 12:05:12 -0700

    task update-1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-
 /**
- * main - This  is the program entry point
- *
- * returns 0 as output
- */
-int main()
+* main - This is the program entry point
+*
+* Description: prints out a string followed by a new line.
+*
+* Return: Always 0 (Success)
+*
+*/
+
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
